afbceed16f09df55bb12756345fed253c217eee3
yandeu <20306025+yandeu@users.noreply.github.com>
2022-05-26 23:40:58 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { h, renderSSR, Helmet, Component } from 'https://deno.land/x/nano_jsx@v0.0.30/mod.ts'
-export { Router, Application } from 'https://deno.land/x/oak@v10.4.0/mod.ts'
+export { h, renderSSR, Helmet, Component } from 'https://deno.land/x/nano_jsx@v0.0.31/mod.ts'
+export { Router, Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts'
