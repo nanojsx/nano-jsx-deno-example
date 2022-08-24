9fd362966c5a09f9f142194e65c502dbf1bae784
yandeu <20306025+yandeu@users.noreply.github.com>
2022-08-24 12:01:36 +0200

    Import from deps.ts

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { h, renderSSR, render, Helmet, Component } from 'https://deno.land/x/nano_jsx@v0.0.33/mod.ts'
+export { h, renderSSR, render, hydrate, Helmet, Component } from 'https://deno.land/x/nano_jsx@v0.0.33/mod.ts'
 export type { FC } from 'https://deno.land/x/nano_jsx@v0.0.33/mod.ts'
--- islands/comments.tsx
+++ islands/comments.tsx
@@ -1,5 +1,5 @@
 import { Comments } from '../components/Comments.tsx'
-import { h, hydrate } from 'https://deno.land/x/nano_jsx@v0.0.33/mod.ts'
+import { h, hydrate } from '../deps.ts'
 
 const comments = ['Hey! This is the first comment.', 'Hi, from another comment!']
 
